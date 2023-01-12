24c96e203cd948a70f08e93185428655512579ce
Girish <girishbhosale345@gmail.com>
2023-01-13 00:24:43 +0530

    Updates

---
+++ precache-manifest.b595aba70c13f1a78480880673b13390.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dadf8f8debc682727c7231585b399b67",
+    "url": "/Keep_app/index.html"
+  },
+  {
+    "revision": "28590cc89be37effedf3",
+    "url": "/Keep_app/static/js/2.e191c4e3.chunk.js"
+  },
+  {
+    "revision": "a6f7244cebef6b3461bd",
+    "url": "/Keep_app/static/js/main.2841ca90.chunk.js"
+  },
+  {
+    "revision": "25a122b29368f541bd31",
+    "url": "/Keep_app/static/js/runtime-main.b583a582.js"
+  }
+]);
